ce7f760f860287c5e6467c0430e9a9d3eb3e40a4
KaiumR <95929203+KaiumR@users.noreply.github.com>
2024-11-05 11:24:59 +0600

    Update index.js
    I have tried to reduce the JavaScript file, to make my code more clean and avoid redundancy.

--- index.js
+++ index.js
@@ -2,35 +2,11 @@ document;
 var dicePlayerOne=Math.floor(Math.random()*6)+1;
 var dicePlayerTwo=Math.floor(Math.random()*6)+1;
 
-if(dicePlayerOne===1){
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-one.png";
-}else if(dicePlayerOne===2){
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-two.png";
-}
-else if(dicePlayerOne===3){
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-three.png";
-}else if(dicePlayerOne===4){
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-four.png";
-}else if(dicePlayerOne===5){
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-five.png";
-}else{
-	document.querySelector(".dice_p1 img").src = "dice-six-faces-six.png";
-}
+var diceForPlayerOne="dice-six-faces-"+dicePlayerOne+".png";
+document.querySelectorAll("img")[0].src =diceForPlayerOne;
 
-if(dicePlayerTwo===1){
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-one.png";
-}else if(dicePlayerTwo===2){
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-two.png";
-}
-else if(dicePlayerTwo===3){
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-three.png";
-}else if(dicePlayerTwo===4){
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-four.png";
-}else if(dicePlayerTwo===5){
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-five.png";
-}else{
-	document.querySelector(".dice_p2 img").src = "dice-six-faces-six.png";
-}
+var diceForPlayerTwo="dice-six-faces-"+dicePlayerTwo+".png";
+document.querySelectorAll("img")[1].src =diceForPlayerTwo;
 
 
 
