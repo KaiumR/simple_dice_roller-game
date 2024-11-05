22a339516bfb313a60617af98875f71af7a05127
KaiumR <95929203+KaiumR@users.noreply.github.com>
2024-11-05 23:25:49 +0600

    Update index.js
    dice-rolling sound has been added.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 document;
-var dicePlayerOne=Math.floor(Math.random()*6)+1;
-var dicePlayerTwo=Math.floor(Math.random()*6)+1;
 
-var diceForPlayerOne="dice-six-faces-"+dicePlayerOne+".png";
-document.querySelectorAll("img")[0].src =diceForPlayerOne;
+function rollTheDice(){
+	var dicePlayerOne=Math.floor(Math.random()*6)+1;
+	var dicePlayerTwo=Math.floor(Math.random()*6)+1;
 
-var diceForPlayerTwo="dice-six-faces-"+dicePlayerTwo+".png";
-document.querySelectorAll("img")[1].src =diceForPlayerTwo;
+	var diceForPlayerOne="dice-six-faces-"+dicePlayerOne+".png";
+	document.querySelectorAll("img")[0].src =diceForPlayerOne;
+
+	var diceForPlayerTwo="dice-six-faces-"+dicePlayerTwo+".png";
+	document.querySelectorAll("img")[1].src =diceForPlayerTwo;
 
 
 
-if(dicePlayerOne>dicePlayerTwo){
-	document.querySelector("h1").innerText="Player 1 wins!";
+	if(dicePlayerOne>dicePlayerTwo){
+		document.querySelector("h1").innerText="Player 1 wins!";
+	}
+	else if(dicePlayerOne<dicePlayerTwo){
+		document.querySelector("h1").innerText="Player 2 wins!";
+	}else{
+		document.querySelector("h1").innerText="Drew!";
+	}
 }
-else if(dicePlayerOne<dicePlayerTwo){
-	document.querySelector("h1").innerText="Player 2 wins!";
-}else{
-	document.querySelector("h1").innerText="Drew!";
-}
+
+document.querySelector("button").addEventListener("click",function(){
+	var audio = new Audio("./rolling_dice_sound.mp3");
+	audio.play();
+	rollTheDice();
+});
